bced16c6af9dd58df60950c248a856adc1640c85
ph2 <hydrogenpan2019@gmail.com>
2019-08-13 10:29:24 +0800

    add ElFormRenderer demo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ElRow>
+      <span>update status:</span>
+      <ElButton @click="updateStatus(Status.normal)">normal</ElButton>
+      <ElButton @click="updateStatus(Status.blocked)">blocked</ElButton>
+      <ElButton @click="updateStatus(Status.limited)">limited</ElButton>
+    </ElRow>
+
+    <ElRow>
+      <ElButton @click="refreshValue">refreshValue</ElButton>
+      <span>switchValue: <span>{{ switchValue }}</span></span>
+    </ElRow>
+
+    <ElDivider>form</ElDivider>
+    <ElFormRenderer
+        :ref="formName"
+        :content="formContent"
+        label-width="120px"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ElFormRenderer from '@femessage/el-form-renderer'
+
+  const Status = {
+    normal: 0,
+    blocked: 1,
+    limited: 2,
+    // ... other status code
+  }
+  Object.freeze(Status)
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    ElFormRenderer
+  },
+
+  data() {
+    const formContentValue = () => {
+      return [
+        {
+          id: 'status',
+          label: '是否启用',
+          type: 'switch',
+          el: {
+            activeText: '启用',
+            activeValue: true,
+            inactiveText: '不启用',
+            inactiveValue: false,
+          },
+          inputFormat(data) {
+            return data.status === Status.normal
+          },
+          outputFormat(value) {
+            // eslint-disable-next-line no-console
+            console.log('outputFormat origin value', value)
+            if (typeof value !== 'boolean') {
+              // eslint-disable-next-line no-console
+              console.warn('The switch value should be boolean')
+            }
+
+            return value
+              ? Status.normal
+              : Status.blocked
+          },
+        }
+      ]
+    }
+
+    return {
+      formName: 'form',
+
+      Status,
+
+      formContent: formContentValue(),
+
+      switchValue: false,
+    }
+  },
+
+  mounted() {
+    this.updateStatus(Status.normal)
+  },
+
+  methods: {
+    getFormRef() {
+      return this.$refs[this.formName]
+    },
+
+    updateStatus(status) {
+      this.getFormRef()
+        .updateForm({
+          status,
+        })
+
+      this.$nextTick(() => {
+        this.refreshValue()
+      })
+    },
+
+    refreshValue() {
+      const formData = this.getFormRef().getFormValue()
+      this.switchValue = formData.status
+    },
+  },
 }
 </script>
 
@@ -21,7 +115,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
